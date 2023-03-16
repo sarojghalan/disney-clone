e6cbf56928e031bb5754b488943c59f0c1bd09a5
Hypekage <jmestmnag@gmail.com>
2023-03-16 20:42:22 +0545

    navbar added with logo and side menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import './App.css';
-import {Routes , Route} from 'react-router-dom'
-import Login from '../src/components/Login/Login'
-
+import React, { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "../src/components/Login/Login";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
+    <Header />
       <Routes>
         <Route exact path='/' element={<Login />}>
         </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
